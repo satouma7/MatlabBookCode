12d2d0f1a9db3c5a14e8fba49070dc3a0be1f80f
satouma7 <67225470+satouma7@users.noreply.github.com>
2020-06-22 18:08:14 +0900

    Update kadai2_12B.m

--- kadai2_12B.m
+++ kadai2_12B.m
@@ -9,13 +9,13 @@ Z=zeros(100,100);
             end
         end
     end
-%     X=50;
-%     for Y=1:100
-%         V=X-50;W=Y-50;
-%         R=sqrt(V*V+W*W);
-%         Theta=atan(W/V)+pi/2;
-%         if (R>30)&&(R<50)
-%             Z(Y,X)=Theta*100/2/pi;
-%         end
-%     end
+    X=50;
+    for Y=1:100
+        V=X-50;W=Y-50;
+        R=sqrt(V*V+W*W);
+        Theta=atan(W/V)+pi/2;
+        if (R>30)&&(R<50)
+            Z(Y,X)=Theta*100/2/pi;
+        end
+    end
 imagesc(Z,[0 100]);set(gca,'YDir','normal');colorbar;
