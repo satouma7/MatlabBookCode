2e7c1fdb1b7a6928ea6e75f178f7efd07938b4a6
satouma7 <67225470+satouma7@users.noreply.github.com>
2020-06-22 18:18:53 +0900

    Update kadai2_2E.m

--- kadai2_2E.m
+++ kadai2_2E.m
@@ -1,5 +1,5 @@
 T=0:1/100:10*pi; 
-R=-T/2/pi+5+rand(1,3142)-0.5;%R=-T/2/pi+4.5+rand(1,size(T,2));    
+R=-T/2/pi+5+rand(1,3142)-0.5;
 X=R.*cos(T); Y=R.*sin(T);
 figure;plot(X,Y);
 figure;plot3(X,Y,T);
