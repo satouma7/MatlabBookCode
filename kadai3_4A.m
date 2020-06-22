163c2b15bcbd649cfc62f303a547ee54a38faed4
satouma7 <67225470+satouma7@users.noreply.github.com>
2020-06-22 18:09:58 +0900

    Update kadai3_4A.m

--- kadai3_4A.m
+++ kadai3_4A.m
@@ -1,8 +1,8 @@
 dt=0.01;Tmax=10000;
-S=zeros(1,Tmax);S(1)=99;%sensitive
-I=zeros(1,Tmax);I(1)=1;%infected
-R=zeros(1,Tmax);R(1)=0;%recovered
-b=0.01;g=0.01;%b=0.001;g=0.01;
+S=zeros(1,Tmax);S(1)=99;
+I=zeros(1,Tmax);I(1)=1;
+R=zeros(1,Tmax);R(1)=0;
+b=0.01;g=0.01;
 for T=1:Tmax-1
     S(T+1)=S(T)+dt*( -b*S(T)*I(T)           );
     I(T+1)=I(T)+dt*(  b*S(T)*I(T) -g*I(T)   );
@@ -10,4 +10,4 @@ for T=1:Tmax-1
 end
 X=1:Tmax;
 plot(X,S,'g--',X,I,'r',X,R,'b:');
-title(strcat('ƒÀ=',num2str(b),' /ƒÁ=',num2str(g),' /‘Š´õÒ”=',num2str(S(1)-S(Tmax)) ));
+title(strcat('Î²=',num2str(b),' /Î³=',num2str(g),' /ç·æ„ŸæŸ“è€…æ•°',num2str(S(1)-S(Tmax)) ));
