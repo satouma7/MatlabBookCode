c845fc108f6e7c5da9a0edb7a828e6c0240a449c
satouma7 <67225470+satouma7@users.noreply.github.com>
2020-06-22 15:42:19 +0900

    Update kadai2_6B.m

--- kadai2_6B.m
+++ kadai2_6B.m
@@ -1,9 +1,9 @@
 A=ceil(3*rand+2);
 B=fix(4*rand+3);
 M=round(10*rand(A,B));
-disp(strcat(num2str(A),'s',num2str(B),'—ñ‚Ìs—ñ'));
+disp(strcat(num2str(A),'è¡Œ',num2str(B),'åˆ—ã®è¡Œåˆ—'));
 for i=1:A
     for j=1:B
-        disp(strcat(num2str(i),'s',num2str(j),'—ñ‚Í', num2str(M(i,j)),'‚Å‚·'));
+        disp(strcat(num2str(i),'è¡Œ',num2str(j),'åˆ—ã¯', num2str(M(i,j)),'ã§ã™'));
     end
 end
