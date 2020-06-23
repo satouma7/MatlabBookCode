7e5b5caa689e43d3246e0cb2064b079b1dcba15b
satouma7 <67225470+satouma7@users.noreply.github.com>
2020-06-23 10:43:35 +0900

    Update kadai2_6B.m

--- kadai2_6B.m
+++ kadai2_6B.m
@@ -1,7 +1,7 @@
 A=ceil(3*rand+2);
 B=fix(4*rand+3);
 M=round(10*rand(A,B));
-disp(strcat(num2str(A),'行',num2str(B),'列の行列'));
+disp(strcat(num2str(A),'行',num2str(B),'列の行列です'));
 for i=1:A
     for j=1:B
         disp(strcat(num2str(i),'行',num2str(j),'列は', num2str(M(i,j)),'です'));
