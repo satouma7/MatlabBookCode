b6bf3390b432b322a08c052ad53efdb3d3dde0cd
satouma7 <makotos@staff.kanazawa-u.ac.jp>
2020-08-18 13:46:59 +0900

    Descriptions added

--- kadai2_2B.m
+++ kadai2_2B.m
@@ -1,5 +1,5 @@
 T=0:1/100:10*pi; 
 R=5;
-X=R*cos(T); Y=R*sin(T); %XをX軸，YをY軸，TをZ軸としてプロット
+X=R*cos(T); Y=R*sin(T);
 figure; plot(X,Y);
-figure; plot3(X,Y,T);
+figure; plot3(X,Y,T);   %XをX軸，YをY軸，TをZ軸としてプロット
