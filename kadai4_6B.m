541811f1a58313bc56aa18d1bed49c5ee687515e
satouma7 <67225470+satouma7@users.noreply.github.com>
2020-09-17 18:19:35 +0900

    Update kadai4_6B.m

--- kadai4_6B.m
+++ kadai4_6B.m
@@ -3,15 +3,15 @@ B=zeros(Xmax,Xmax,Tmax);W=B;D=B;R=zeros(Xmax,Xmax);
 dt=0.1;d=1;k=0.1;a=1;
 cb=zeros(Xmax,Xmax);cb(51:100,41:60)=0.5;
 cw=zeros(Xmax,Xmax);cw(1:50,41:60)=0.5;
-clone=ones(5,5);%‚à‚µ‚­‚Í clone=[0 1 1 1 0; 1 1 1 1 1; 1 1 1 1 1; 1 1 1 1 1; 0 1 1 1 0];
-R(31:35,3:7)=clone;R(31:35,23:27)=clone;    %Šˆ«‰»ó—e‘ÌR‚Ì•ª•z‚ğŒˆ‚ß‚é
-R(31:35,43:47)=clone;R(31:35,63:67)=clone;  %Šˆ«‰»ó—e‘ÌR‚Ì•ª•z‚ğŒˆ‚ß‚é
+clone=ones(5,5);%ã‚‚ã—ãã¯ clone=[0 1 1 1 0; 1 1 1 1 1; 1 1 1 1 1; 1 1 1 1 1; 0 1 1 1 0];
+R(31:35,3:7)=clone;R(31:35,23:27)=clone;    %æ´»æ€§åŒ–Dppå—å®¹ä½“ã®ç©ºé–“åˆ†å¸ƒRã‚’æ±ºã‚ã‚‹
+R(31:35,43:47)=clone;R(31:35,63:67)=clone;  %æ´»æ€§åŒ–Dppå—å®¹ä½“ã®ç©ºé–“åˆ†å¸ƒRã‚’æ±ºã‚ã‚‹
 for T=1:Tmax
     B(:,:,T+1)=dt*(4*d*del2(B(:,:,T))-k*B(:,:,T)+cb)+B(:,:,T);
     W(:,:,T+1)=dt*(4*d*del2(W(:,:,T))-k*W(:,:,T)+cw)+W(:,:,T);
-    D(:,:,T+1)=dt*(a*(B(:,:,T)+R).*W(:,:,T)-k*D(:,:,T))+D(:,:,T);%B, R, W‚É‚æ‚éD‚ÌY¶E•ª‰ğ‚ÌŒvZ
+    D(:,:,T+1)=dt*(a*(B(:,:,T)+R).*W(:,:,T)-k*D(:,:,T))+D(:,:,T);%B, R, Wã«ã‚ˆã‚‹Dã®ç”£ç”Ÿãƒ»åˆ†è§£ã®è¨ˆç®—
 end
-figure('Position',[0 400 1000 250]);    %ƒEƒBƒ“ƒhƒE‚ÌƒTƒCƒY‚ÆˆÊ’u‚Í“K‹X’²ß‚µ‚Ä‰º‚³‚¢
+figure('Position',[0 400 1000 250]);    %ã‚¦ã‚£ãƒ³ãƒ‰ã‚¦ã®ã‚µã‚¤ã‚ºã¨ä½ç½®ã¯é©å®œèª¿ç¯€ã™ã‚‹
 for T=1:20:Tmax
     subplot(1,3,1);imagesc(B(:,:,T),[0 1]);set(gca,'YDir','normal');colorbar;
     subplot(1,3,2);imagesc(W(:,:,T),[0 1]);set(gca,'YDir','normal');colorbar;
