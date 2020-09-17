32691ca69584df869253bcc85a206f479c37022e
satouma7 <67225470+satouma7@users.noreply.github.com>
2020-09-17 16:19:35 +0900

    Update kadai3_6eq.m

--- kadai3_6eq.m
+++ kadai3_6eq.m
@@ -1,4 +1,4 @@
-function dL=kadai3_6eq(T,L) %関数名'kadai3_5eq'をファイル名と一致させる
+function dL=kadai3_6eq(T,L) %関数名'kadai3_6eq'をファイル名と一致させる
     global a b c d      %a, b, c, dをグローバル変数と宣言
     dL=zeros(2,1);
     dL(1)=a*L(1)      -b*L(1)*L(2)  ;
