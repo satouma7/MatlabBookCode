50e26287868b309d3f55dab165a2ad4f5bff82ba
satouma7 <67225470+satouma7@users.noreply.github.com>
2020-09-17 13:55:04 +0900

    Update kadai3_5A.m

--- kadai3_5A.m
+++ kadai3_5A.m
@@ -1,7 +1,7 @@
 dt=0.01;Tmax=10000;
-S=zeros(1,Tmax);S(1)=99;%–¢Š´õŽÒ
-I=zeros(1,Tmax);I(1)=1;%Š´õŽÒ
-R=zeros(1,Tmax);R(1)=0;%‰ñ•œŽÒ
+S=zeros(1,Tmax);S(1)=99;%未感染者
+I=zeros(1,Tmax);I(1)=1;%感染者
+R=zeros(1,Tmax);R(1)=0;%回復者
 b=0.01;g=0.01;
 for T=1:Tmax-1
     S(T+1)=S(T)+dt*( -b*S(T)*I(T)           );
@@ -10,4 +10,4 @@ for T=1:Tmax-1
 end
 X=1:Tmax;
 plot(X,S,'g--',X,I,'r',X,R,'b:');
-title(strcat('ƒÀ=',num2str(b),' /ƒÁ=',num2str(g),' /‘Š´õŽÒ”=',num2str(S(1)-S(Tmax)) ));
+title(strcat('β=',num2str(b),' /γ=',num2str(g),' /‘創感染者数”=',num2str(S(1)-S(Tmax)) ));
