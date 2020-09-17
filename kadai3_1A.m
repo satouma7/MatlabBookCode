fc8ae388dbe33f0dd73532a0e7e4e40793ec2588
satouma7 <67225470+satouma7@users.noreply.github.com>
2020-09-17 13:58:36 +0900

    Rename kadai3_1A.m to kadai3_2A.m

--- kadai3_1A.m
+++
@@ -1,7 +0,0 @@
-Tmax=5000;dt=0.01;
-p=1;k=0.1;
-E=zeros(1,Tmax);
-for T=1:Tmax-1      %Tを1からTmax-1まで変化させる
-    E(1,T+1)=(p-k*E(1,T))*dt+E(1,T);    %オイラー法に従いEの値を計算 E(T+1)=(p-k*E(T))*dt+E(T);　としても良い
-end
-plot(1:Tmax,E(1,:));    %plot(1:Tmax, E); としても良い
