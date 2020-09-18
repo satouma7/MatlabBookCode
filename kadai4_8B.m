30d1eac3b1b95c06989b0e442d00ad7ca9354e1e
satouma7 <67225470+satouma7@users.noreply.github.com>
2020-09-18 09:42:36 +0900

    Update kadai4_8B.m

--- kadai4_8B.m
+++ kadai4_8B.m
@@ -1,13 +1,13 @@
-Tmax=10000;Xmax=100;        %‚à‚µ‚­‚ÍTmax=5000;
+Tmax=10000;Xmax=100;        %ã‚‚ã—ãã¯Tmax=5000;
 dt=0.02;dx=1;dx2=dx*dx;
 V=zeros(Xmax,Xmax,Tmax);W=zeros(Xmax,Xmax,Tmax);
-I=zeros(Xmax,Xmax);I(50:51,50:51)=1;    %“d—¬“ü—ÍI‚Ì‹óŠÔƒpƒ^[ƒ“‚ğİ’è
+I=zeros(Xmax,Xmax);I(50:51,50:51)=1;    %é›»æµå…¥åŠ›Iã®ç©ºé–“ãƒ‘ã‚¿ãƒ¼ãƒ³ã‚’è¨­å®š
 a=0.7;b=0.8;c=10;d=1;
 for T=1:Tmax-1
         V(:,:,T+1)=V(:,:,T)+dt*(  c*( - (V(:,:,T).^3)/3+ V(:,:,T) -W(:,:,T) + I ) + 4*d*del2(V(:,:,T)) );
         W(:,:,T+1)=W(:,:,T)+dt*(V(:,:,T)-b*W(:,:,T)+a)/c;
 end
-figure('Position',[0 300 1000 400]);    %ƒEƒBƒ“ƒhƒE‚ÌƒTƒCƒY‚ÆˆÊ’u‚Í“K‹X’²ß‚µ‚Ä‰º‚³‚¢
+figure('Position',[0 300 1000 400]);    %ã‚¦ã‚£ãƒ³ãƒ‰ã‚¦ã®ã‚µã‚¤ã‚ºã¨ä½ç½®ã¯é©å®œèª¿ç¯€ã™ã‚‹
 for T=1:100:Tmax
     subplot(1,2,1);imagesc(V(:,:,T),[-2 2]);set(gca,'YDir','normal');colorbar;
     subplot(1,2,2);imagesc(W(:,:,T),[-1 1]);set(gca,'YDir','normal');colorbar;
